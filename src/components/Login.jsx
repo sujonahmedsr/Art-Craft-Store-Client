8161df491cde02416237a8c55524979b2b17ed15
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-04-26 09:49:15 +0600

    hero section added with slider

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,6 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "./AuthProvider/AuthProvider";
 import { Link } from "react-router-dom";
-import { signInWithPopup } from "firebase/auth";
-import auth from "./firebase/firebase";
-import { GoogleAuthProvider } from "firebase/auth/cordova";
 
 const Login = () => {
     const { signInMethod } = useContext(AuthContext)
@@ -13,9 +10,7 @@ const Login = () => {
         const from = e.target;
         const email = from.email.value;
         const password = from.password.value;
-
-
-
+        
         signInMethod(email, password)
             .then(res => {
                 console.log(res.user);
@@ -24,20 +19,9 @@ const Login = () => {
                 console.log(error);
                 return setError('Invalid email or password');
             })
-
     }
 
-    const googleProvider = new GoogleAuthProvider();
-
-    const signInWithGoogle = () => {
-        signInWithPopup(auth, googleProvider)
-            .then(res => {
-                console.log(res.user);
-            })
-            .catch(error => {
-                console.log(error.message);
-            })
-    }
+    
     return (
         <div className="py-20">
             <div className="w-full max-w-md container mx-auto p-8 space-y-3 rounded-xl text-gray-900 bg-gray-100 shadow-2xl">
@@ -65,7 +49,7 @@ const Login = () => {
                     <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
                 </div>
                 <div className="flex justify-center space-x-4">
-                    <button onClick={signInWithGoogle} aria-label="Log in with Google" className="p-3 rounded-sm">
+                    <button aria-label="Log in with Google" className="p-3 rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
