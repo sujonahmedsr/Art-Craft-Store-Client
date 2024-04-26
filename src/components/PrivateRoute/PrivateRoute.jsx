c1f5e5f6c007b0994c3615e5ef6c6dac4a9c4e18
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-04-26 10:11:25 +0600

    create signup page with create user authentication

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,21 +2,11 @@ import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
-import { Audio } from 'react-loader-spinner'
-
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
     if (loading) {
-             <Audio
-                height="100"
-                width="100"
-                radius="10"
-                color="green"
-                ariaLabel="loading"
-                wrapperStyle
-                wrapperClass
-            />
+             <p className="h-[90vh] bg-red-600">Loading...</p>
     }
 
     if (user) {
